fea3555509af41339c2e05c1a821b22739a6f38b
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2022-10-22 21:05:00 +0200

    fix main funcition publish

--- app/main.py
+++ app/main.py
@@ -11,13 +11,9 @@ def main():
         db=MongoClient(config("MONGODB_URI")).get_default_database(),
         cboe_collection_name=config("CBOE_COLLECTION_NAME"),
         spot_collection_name=config("SPOT_COLLECTION_NAME"),
+        open_interest_collection_name=config("OPEN_INTEREST_COLLECTION_NAME"),
     )
-    query_manager.spot_queries.insert_many(
-        document={
-            "current_price": current_price
-        },
-        now = now
-    )
+    query_manager.open_interest.insert(now)
     print("success")
 
 
